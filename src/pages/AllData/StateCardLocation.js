8b479f816f9de64a72283afaf0fab16ef0abcb7d
Rendi Simamora <rendisimamora7127@gmail.com>
2018-01-09 23:02:28 +0700

    State Card Location

---
+++ src/pages/AllData/StateCardLocation.js
@@ -0,0 +1,38 @@
+export const StateCardLocation = [
+    {
+      number: "1",
+      name: "HONDA HR-V",
+      image: "http://www.pngmart.com/files/4/Car-PNG-HD.png",
+      year: '2010 ',
+      type: 'Mobil',
+      location: 'Jakarta',
+      date: ' 1 Januari 2017',
+      time: ' 13.00 - Selesai',
+      button: 'BID NOW',
+      key: 1
+    },
+    {
+      number: "2",
+      name: "HONDA HR-V",
+      image: "http://www.pngmart.com/files/4/Car-PNG-HD.png",
+      year: '2010 ',
+      type: 'Mobil',
+      location: 'Jakarta',
+      date: ' 1 Januari 2017',
+      time: ' 13.00 - Selesai',
+      button: 'BID NOW',
+      key: 2
+    },
+    {
+      number: "3",
+      name: "HONDA HR-V",
+      image: "http://www.pngmart.com/files/4/Car-PNG-HD.png",
+      year: '2010 ',
+      type: 'Mobil',
+      location: 'Jakarta',
+      date: ' 1 Januari 2017',
+      time: ' 13.00 - Selesai',
+      button: 'BID NOW',
+      key: 3
+    },
+  ]
--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
-import { Image, Grid, Row, Col, Button } from 'react-bootstrap'
+import { Image, Grid, Row, Col } from 'react-bootstrap'
 import Banner from '../Components/Banner'
 import Card1 from '../Components/Card1'
 import { StateCard1 } from '../AllData/StateCard1'
+import { StateCardLocation } from '../AllData/StateCardLocation'
+import JadwalLelang from '../Components/JadwalLelang'
 import TabLeft from '../Components/TabLeft'
+
+//Icon & Image
 import GoCalendar from 'react-icons/lib/go/calendar'
-import Car5 from '../../assets/image/car5.png'
 import Map from '../Components/Map'
 import Logo from '../../assets/image/logo-dummy.png'
 
@@ -53,74 +56,25 @@ export class Index extends Component {
                     <hr/>
                   </div>
                   <Row>
-                    <Col xs={12} md={12}>
-                      <Row style={{border:'1px solid #eb5f02',marginTop:'2%'}}>
-                        <Col xs={4} md={2}>
-                          <div className='lelang-lot'>
-                            <h2><center> LOT <br/> 1</center></h2>
-                          </div>
-                        </Col>
-                        <Col xs={4} md={3}>
-                          <Image src={Car5} width='100%'/>
-                        </Col>
-                        <Col xs={4} md={7}>
-                          <div className='content-lelang'>
-                            <p className='sub-title'> Toyota Avanza </p>
-                            <p className='year'> Tahun 2000 </p>
-                            <p className='type'> Jenis Kendaraan : Mobil </p>
-                            <p className='loc'> Lokasi : Jakarta | Tanggal : 1 Januari 2018 | Jam : 13.00 - Selesai </p>
-                            <Button> Info Lengkap </Button> 
-                          </div>
-                        </Col>
-                      </Row>
+                  {StateCardLocation.map((data, index) => (
+                    <Col md={12} key={data.key}>
+                      
+                      <JadwalLelang 
+                        number={data.number}
+                        name={data.name}
+                        year={data.year}
+                        type={data.type}
+                        location={data.location}
+                        date={data.date}
+                        time={data.time}
+                        image={data.image}
+                        price={data.price}
+                        button={data.button}
+                      />
                     </Col>
-                    
-                    <Col xs={12} md={12}>
-                      <Row style={{border:'1px solid #eb5f02',marginTop:'2%'}}>
-                        <Col xs={4} md={2}>
-                          <div className='lelang-lot'>
-                            <h2><center> LOT <br/> 2</center></h2>
-                          </div>
-                        </Col>
-                        <Col xs={4} md={3}>
-                          <Image src={Car5} width='100%'/>
-                        </Col>
-                        <Col xs={4} md={7}>
-                          <div className='content-lelang'>
-                            <p className='sub-title'> Toyota Avanza </p>
-                            <p className='year'> Tahun 2000 </p>
-                            <p className='type'> Jenis Kendaraan : Mobil </p>
-                            <p className='loc'> Lokasi : Jakarta | Tanggal : 1 Januari 2018 | Jam : 13.00 - Selesai </p>
-                            <Button> Info Lengkap </Button> 
-                          </div>
-                        </Col>
-                      </Row>
-                    </Col>
-
-                    <Col xs={12} md={12}>
-                      <Row style={{border:'1px solid #eb5f02',marginTop:'2%'}}>
-                        <Col xs={4} md={2}>
-                          <div className='lelang-lot'>
-                            <h2><center> LOT <br/> 3</center></h2>
-                          </div>
-                        </Col>
-                        <Col xs={6} md={3}>
-                          <Image src={Car5} width='100%'/>
-                        </Col>
-                        <Col xs={6} md={7}>
-                          <div className='content-lelang'>
-                            <p className='sub-title'> Toyota Avanza </p>
-                            <p className='year'> Tahun 2000 </p>
-                            <p className='type'> Jenis Kendaraan : Mobil </p>
-                            <p className='loc'> Lokasi : Jakarta | Tanggal : 1 Januari 2018 | Jam : 13.00 - Selesai </p>
-                            <Button> Info Lengkap </Button> 
-                          </div>
-                        </Col>
-                      </Row>
-                    </Col>
+                  ))}
                   </Row>
                   <a href='/'><p className='lihatJadwal'> Lihat Semua Jadwal </p></a>
-                  
                 </Grid>
               </div>
 
