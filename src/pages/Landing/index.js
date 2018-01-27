2093f4ead583ec2df5bbaa0c9b18c8ab21fdf4b6
Rendi Simamora <rendisimamora7127@gmail.com>
2018-01-27 15:13:37 +0700

    Delete Whatsapp anchor

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -204,7 +204,6 @@ export class Index extends Component {
                   </Row>
                 </Grid>
               </div>
-              <a href="whatsapp://send?text=Hello Circledoo&phone=+6281999197186">Hello world </a>
           </div>
         )
     }
