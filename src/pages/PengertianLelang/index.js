1f35e01bf6053245b67d89bb562b6cee9ac202f4
Rendi Simamora <rendisimamora7127@gmail.com>
2018-03-06 16:40:44 +0700

    Add border in pengertian-lelang

--- src/pages/PengertianLelang/index.js
+++ src/pages/PengertianLelang/index.js
@@ -19,20 +19,22 @@ export class Index extends Component {
               style={{ width: "100%" }}
             />
             </Col>
-            <Col md={8}>
-              <p className="text-center">
-                Lelang adalah proses membeli dan menjual barang atau jasa dengan
-                cara menawarkan kepada penawar, menawarkan tawaran harga lebih
-                tinggi. dan kemudian menjual barang kepada penawar harga
-                tertingi, Lelang di indonesia masih tergolong proses yang tidak
-                familiah. produk yang biasa dilakukan lelang adalah kendaraan,
-                properti, dan komoditi. pada dasarnya semua barang dapat
-                dilakukan lelang. ketika ada kebutuhan atas penjualan dengan
-                cepat. atau harga tertingi, atau penjualan dalam skala banyak,
-                maka penjualan melalui lelang adalah cara yang paling tepat
-              </p>
+            <Col md={10}>
+              <div style={{border:'1px solid #ccc', padding:10}}>
+                <h3> Apa itu lelang ? </h3>
+                <p>
+                  Lelang adalah proses membeli dan menjual barang atau jasa dengan
+                  cara menawarkan kepada penawar, menawarkan tawaran harga lebih
+                  tinggi. dan kemudian menjual barang kepada penawar harga
+                  tertingi, Lelang di indonesia masih tergolong proses yang tidak
+                  familiah. produk yang biasa dilakukan lelang adalah kendaraan,
+                  properti, dan komoditi. pada dasarnya semua barang dapat
+                  dilakukan lelang. ketika ada kebutuhan atas penjualan dengan
+                  cepat. atau harga tertingi, atau penjualan dalam skala banyak,
+                  maka penjualan melalui lelang adalah cara yang paling tepat
+                </p>
+              </div>
             </Col>
-            <Col md={2} />
           </Row>
         </Grid>
       </div>
