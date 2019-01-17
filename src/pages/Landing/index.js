2f94514eef84f6820a94a2867b380988f7d63853
Andika <andika@twiscode.com>
2019-01-17 16:51:49 +0700

    autoplay and size

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -833,7 +833,7 @@ class Index extends Component {
           <div className="sticky-video">
             <Button className="btn-delay" type="primary" shape="circle" icon="close" onClick={()=>{this.setState({video:false})}} />
             <div class="video-container">
-            <iframe className="content-media__object" id="featured-video" width="560" height="315" src="https://www.youtube.com/embed/pua_QFrmCqM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe className="content-media__object" id="featured-video" width="444" height="255" src="https://www.youtube.com/embed/pua_QFrmCqM?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
            </div>
           </div>
         }
