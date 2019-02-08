10cc4bacbed982b9fe55c9be888700c43f3e2368
Andika <ahmad.andika212@gmail.com>
2019-02-08 14:38:53 +0700

    build map

--- src/pages/Lokasi/index.js
+++ src/pages/Lokasi/index.js
@@ -18,7 +18,7 @@ class Index extends Component {
               </div>
               <Row>
                 <Col xs={12} md={12}>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.446550562986!2d106.73474321456337!3d-6.204675895508321!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x8b09ce89d986319!2sPt.+Digital+Sarana+Legoas!5e0!3m2!1sid!2sid!4v1547192351344" title="map" style={{marginLeft:"10%", width:"80%", height:600}} frameborder="0" allowfullscreen></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.4470743860265!2d106.73473651476907!3d-6.20460629550834!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f7b1852c75a1%3A0x6a976ae8016423df!2sPT.+DIGITAL+SARANA+LEGOAS!5e0!3m2!1sid!2sid!4v1549611252590" title="map" style={{marginLeft:"10%", width:"80%", height:600}} frameborder="0" allowfullscreen></iframe>
                   <div className="wrap-location">
                     <p className="sub-location">PT Digital Sarana Legoas</p>
                   </div>
