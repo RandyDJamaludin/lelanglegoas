6475cfd1d535043482410933510ffeb2081faba8
Andika <ahmad.andika212@gmail.com>
2019-04-15 16:48:46 +0700

    halaman cari mobil

--- src/pages/Home/HomeDescription.js
+++ src/pages/Home/HomeDescription.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Grid, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import Logo1 from "../../assets/image/home/aset-01.png";
 import Logo2 from "../../assets/image/home/aset-02.png";
 import Logo3 from "../../assets/image/home/aset-03.png";
@@ -28,8 +29,10 @@ class HomeDescription extends Component {
         <Grid>
           <Row>
             <Col md={3} className="text-center">
-              <img width={150} height={150} src={Logo1} />
-              <div className="base-color w700">Cari Mobil/Motor</div>
+              <Link to="/cari-mobil">
+                <img width={150} height={150} src={Logo1} />
+                <div className="base-color w700">Cari Mobil/Motor</div>
+              </Link>
             </Col>
             <Col md={3} className="text-center">
               <img width={150} height={150} src={Logo2} />
